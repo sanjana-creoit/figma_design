6eb84574e9cbc3bcc1f47cc1f838b48641448d68
sanjana-creoit <sanjana@creoit.com>
2023-08-18 12:49:26 +0530

    Add Search Function - add search contact in contactPage to display searched list

--- lib/contactPageController.dart
+++ lib/contactPageController.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:figma_design/Model/database_helper.dart';
 import 'package:figma_design/ProfilePage.dart';
 import 'package:flutter/cupertino.dart';
@@ -12,8 +13,18 @@ class ContactPageController extends GetxController {
   Contacts contacts = Contacts();
   RxBool isLoading = false.obs;
 
+  Timer? debounce;
+  List<Contacts> filteredResult = List.empty(growable: true);
+
   @override
-  onInit() async{
+  void dispose() {
+    debounce?.cancel();
+    super.dispose();
+  }
+
+
+  @override
+  onInit() async {
     databaseHelper.getAllContact();
     print("Contact List ...");
     super.onInit();
@@ -32,19 +43,19 @@ class ContactPageController extends GetxController {
         itemBuilder: (context, index) {
           return GestureDetector(
             onTap: () async {
-              ProfilePageController profilePageController = Get.put(ProfilePageController());
+              ProfilePageController profilePageController =
+                  Get.put(ProfilePageController());
               profilePageController.contacts = contactList[index];
-           var result = await  Get.to(() => ProfilePage());
-           if(result == true){
-             getAllContacts();
-           }
+              var result = await Get.to(() => ProfilePage());
+              if (result == true) {
+                getAllContacts();
+              }
             },
             child: Row(
               children: [
                 Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child:
-                  Image.asset(
+                  child: Image.asset(
                     contactList[index].image ?? "assets/images/contact.png",
                     height: 40,
                     width: 40,
@@ -81,5 +92,34 @@ class ContactPageController extends GetxController {
             .toList();
     alphabets.refresh();
     print("alphabets length = ${alphabets.length}");
+  }
+
+  void displaySearchItem() {
+    List<Contacts> searchResult = [];
+    if (search.text.isEmpty) {
+      searchResult = contactList;
+    } else {
+      searchResult = contactList
+          .where((e) => e.firstName != null && e.firstName!.contains(search.text))
+          .toList();
+    }
+    filteredResult = searchResult;
+    alphabets.value =
+        List.generate(26, (index) => String.fromCharCode(index + 65))
+            .map((e) =>
+        filteredResult.any((c) => c.firstName?.startsWith(e) == true)
+            ? Alphabets(
+            e,
+            filteredResult
+                .where((cl) => cl.firstName?.startsWith(e) == true)
+                .toList())
+            : null)
+            .toList()
+            .where((element) => element != null)
+            .map((e) => e as Alphabets)
+            .toList();
+    alphabets.refresh();
+    print("alphabets length = ${alphabets.length}");
+    print("FILTERED RESULT: $filteredResult");
   }
 }
